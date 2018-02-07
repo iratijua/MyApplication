e334dbe1b9b6ae072c12e72f84a6415e48d787af
sasiroot <sasiroot@NA106-US07.ikaira.iurretalhi.eus>
2018-02-07 09:02:28 +0100

    print Hello message when button is pressed

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -58,7 +58,7 @@ public class MainFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+           System.out.println("Hello world");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
